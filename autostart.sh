f0027f0696d13aeba22d12d29200936721d6bc34
darekem73 <darek.mastalerz@gmail.com>
2019-01-25 00:01:30 +0000

    Add files via upload

--- autostart.sh
+++ autostart.sh
@@ -1,1 +1,10 @@
-nitrogen --restore
+nitrogen --restore &
+conky -d -c ~/.conkyrc-stat
+nm-applet &
+# blueman-applet &
+# system-config-printer-applet &
+unclutter -idle 1 -root &
+mopidy &
+# compton &
+setxkbmap -option caps:escape
+slstatus &
