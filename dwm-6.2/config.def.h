e0ede8b29f5c74f7efaedcb1c44f67f18691fa52
darekem73 <darek.mastalerz@gmail.com>
2019-02-08 20:07:58 +0100

    Add files via upload

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -77,7 +77,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "terminator", NULL };
+//static const char *termcmd[]  = { "terminator", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *screenlockcmd[]  = { "screenlock", NULL };
 static const char mixername[] = "alsamixer";
 static const char *mixercmd[] = { "st", "-t", mixername, "-e", mixername, NULL };
