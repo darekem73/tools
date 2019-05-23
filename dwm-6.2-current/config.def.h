35f8b8fc1e6496e65acbf8c562ee8120fbab48c4
Dariusz Mastalerz <darek@core>
2019-05-23 19:57:58 +0200

    dwm

--- dwm-6.2-current/config.def.h
+++ dwm-6.2-current/config.def.h
@@ -132,7 +132,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_w,      	setlayout,      {.v = &layouts[6]} },
 	{ MODKEY,                       XK_v,      	setlayout,      {.v = &layouts[10]} },
 	{ MODKEY,                       XK_g,           setlayout,      {.v = &layouts[11]} },
-	{ MODKEY,                       XK_space,  	setlayout,      {0} },
+	// { MODKEY,                       XK_space,  	setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  	togglefloating, {0} },
 	{ MODKEY,                       XK_0,      	view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      	tag,            {.ui = ~0 } },
